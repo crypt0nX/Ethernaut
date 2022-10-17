b6f2f3cb8efc0af2a4f9395480482e9cc25e463b
crypt0n <spencer_clay@foxmail.com>
2022-10-17 15:51:25 +0800

    add Hello Ethernaut

---
+++ Hello Ethernaut/solve.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.4.21;
+
+
+/*
+contract.info()
+"You will find what you need in info1()."
+contract.info1()
+"Try info2(), but with \"hello\" as a parameter."
+contract.info2("hello")
+"The property infoNum holds the number of the next info method to call."
+contract.infoNum()
+await contract.info()
+"You will find what you need in info1()."
+await contract.info42()
+"theMethodName is the name of the next method."
+await contract.theMethodName()
+"The method name is method7123949."
+await contract.method7123949()
+"If you know the password, submit it to authenticate()."
+await contract.password()
+await contract.password
+function call()
+"ethernaut0"
+await contract.authenticate("ethernaut0") 
+
+
+*/
