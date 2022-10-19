adfe7d0ac1290fedb6ec525ded0d8d105e9e9fc0
crypt0n <spencer_clay@foxmail.com>
2022-10-19 11:57:04 +0800

    add Privacy

---
+++ Privacy/solve.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+/*
+web3.eth.getStorageAt("0xb843795887C7624Ac29c348f1C81e66b997b422E", 5).then(console.log);
+*/
+
+contract EXP{
+    bytes32 public key = 0x16e791c7eb108a8102c8494d1e668090d80c119837cc7938ec7c14f3996dc132;
+    bytes16 public key2 = bytes16(key);
+}
